9987fb8942edd30ce44b58e00ae2e8213c0a60e2
julianknutsen <julianknutsen@users.noreply.github.com>
2026-04-13 05:22:59 +0000

    fix: honor no-db target precedence consistently
    (cherry picked from commit 3a6ed2eac2fa3e4d2294c8fd65d06433fee5abce)

--- cmd/bd/dolt.go
+++ cmd/bd/dolt.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
+	"github.com/steveyegge/beads/internal/beads"
 	"github.com/steveyegge/beads/internal/config"
 	"github.com/steveyegge/beads/internal/configfile"
 	"github.com/steveyegge/beads/internal/doltserver"
@@ -1245,7 +1246,7 @@ func init() {
 }
 
 func selectedDoltBeadsDir() string {
-	beadsDir := selectedNoDBBeadsDir(nil)
+	beadsDir := beads.FindBeadsDir()
 	if beadsDir == "" {
 		return ""
 	}
